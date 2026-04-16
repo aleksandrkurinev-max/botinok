6c74b31608ab3bb63d4d3e60718fe306c26d1106
aleksandrkurinev-max <aleksandrkurinev@gmail.com>
2026-04-17 04:22:35 +0500

    Update telegram_bot.py

--- telegram_bot.py
+++ telegram_bot.py
@@ -10,7 +10,7 @@ logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
 # Конфигурация
-TOKEN = "8339819988:AAE35waTsfbWJU7s9i92TCxyTq6HI-lH_g8"
+TOKEN = "8339819988:AAGeVKmrVlx5ckXvDIFQc2DGwIr2_RnuhNc"
 
 # Инициализация бота
 bot = Bot(token=TOKEN)
